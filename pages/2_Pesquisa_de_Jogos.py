11bb70270ac8f50797e77ef790451135181a0502
raysonbs <rayson.bs@hotmail.com>
2025-11-08 20:49:31 -0400

    ajuste coluna 2

--- pages/2_Pesquisa_de_Jogos.py
+++ pages/2_Pesquisa_de_Jogos.py
@@ -205,7 +205,7 @@ if current_df_page_specific is not None:
 
     filtered_df = current_df_page_specific.copy()
     filtered_df = filtered_df[['liga_nome','data_sem_fuso','home_name','away_name','homeGoalCount','awayGoalCount',
-                             'odds_ft_1','odds_ft_2','Media_Gols_F_H','Media_Gols_F_H','CV_Gols_F_H','CV_Gols_F_A']]
+                             'odds_ft_1','odds_ft_2','Media_Gols_F_H','Media_Gols_F_A','CV_Gols_F_H','CV_Gols_F_A']]
 
     # --- Filtro de Data ---
     if 'data_sem_fuso' in filtered_df.columns and not filtered_df['data_sem_fuso'].isnull().all():
