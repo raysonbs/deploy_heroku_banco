88faf5f035857a8abbe05c1bfbe63530e58403db
raysonbs <rayson.bs@hotmail.com>
2025-11-17 23:12:00 -0400

    créditos2

--- pages/4_Resultados.py
+++ pages/4_Resultados.py
@@ -6,4 +6,9 @@ st.set_page_config(layout="wide") # Opcional, mas recomendado para usar a largur
 
 st.title("Em Construção")
 
-st.markdown("Página em Construção")
+
+
+st.markdown("""
+# Página em Construção 
+...
+""", unsafe_allow_html=True)
