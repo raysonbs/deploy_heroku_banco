1a8235b7f2ee658f706a006d408fd33eaf555842
raysonbs <rayson.bs@hotmail.com>
2025-11-17 23:07:15 -0400

    créditos

---
+++ pages/4_Resultados.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.set_page_config(layout="wide") # Opcional, mas recomendado para usar a largura total da página
+
+st.title("Em Construção")
+
+st.markdown("Página em Construção")
---
+++ pages/7_Créditos.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.set_page_config(layout="wide") # Opcional, mas recomendado para usar a largura total da página
+
+st.title("Créditos do Sistema")
+
+
+
+st.markdown("Produzido por Rayson Bernardo")
